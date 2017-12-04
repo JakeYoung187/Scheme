7d91d9f8262667c92cb3c8f5ca623afde8a03fe5
JakeYoung187 <youngjac@mail.gvsu.edu>
2017-12-04 10:38:46 -0500

    Added actual scheme file

---
+++ cash-register.scm
@@ -0,0 +1,11 @@
+
+;main method introduces and sends it to register (which loops until done.) Then it is sent to a totaling function
+;that adds the taxes and returns the final amount
+
+(define (register total input)
+	;print enter a value: 
+	;read in and save to input
+	;check if zero then send to finish
+	;update total
+	;return total (or recursively call)
+	)
